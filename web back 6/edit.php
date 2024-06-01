e215a995e7a4fd486caae7829b6266d9ac1ddb28
valeria <pechkanovalera@gmail.com>
2024-06-02 00:02:52 +0300

    j

--- web back 6/edit.php
+++ web back 6/edit.php
@@ -2,8 +2,18 @@
 header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include('data.php');
-   
-    if ($_POST['action'] == 'delete') {
+    if ($_POST['action'] == 'change') {
+        try {
+            $id = $_POST['id'];
+            $stmt = $db->prepare("update users SET name = :name, phone = :phone, email = :email, date=:date,  gender = :gender, biography = :biography where id = :id");
+            $stmt -> execute(['name'=>$_POST['name'],'phone'=>$_POST['phone'], 'email'=>$_POST['email'],'date'=>$_POST['date'],'gender'=>$_POST['gender'],'biography'=>$_POST['biography'], 'id' => $id]);
+        }
+        catch(PDOException $e){
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    }
+    else if ($_POST['action'] == 'delete') {
         try {
             $id = $_POST['id'];
             $stmt = $db->prepare("DELETE FROM users where id = ?");
@@ -19,17 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
        
     }
-    if ($_POST['action'] == 'change') {
-        try {
-            $id = $_POST['id'];
-            $stmt = $db->prepare("update users SET name = :name, phone = :phone, email = :email, date=:date,  gender = :gender, biography = :biography where id = :id");
-            $stmt -> execute(['name'=>$_POST['name'],'phone'=>$_POST['phone'], 'email'=>$_POST['email'],'date'=>$_POST['date'],'gender'=>$_POST['gender'],'biography'=>$_POST['biography'], 'id' => $id]);
-        }
-        catch(PDOException $e){
-            print('Error : ' . $e->getMessage());
-            exit();
-        }
-    }
+    
     setcookie('save', '1');
     header('Location: admin.php');
 }
