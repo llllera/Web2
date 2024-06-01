c8b91ebb31ae3a7d84a9543dc6633fb16f8a6ad5
valeria <pechkanovalera@gmail.com>
2024-06-01 18:21:36 +0300

    fff

--- web back 5/index1.php
+++ web back 5/index1.php
@@ -50,10 +50,11 @@ if (!empty($messages)) {
         <li>
           <div class="date">
             <span>Год рождения:</span>
-            <select name="date">  <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>"
+            <select name="date">  <?php if ($errors['date']) {print 'class="error"';} ?> 
               <?php 
                 for ($i = 2022; $i >= 1922; $i--) {
-                  printf('<option value="%d">%d год</option>', $i, $i);
+                  $selected = ($i == $values['date']) ? 'selected' : '';
+                  printf('<option value="%d" %s>%d год</option>', $i,$selected, $i);
                 }
               ?>
             </select>
@@ -61,39 +62,39 @@ if (!empty($messages)) {
         </li>
         <li>
             Пол<br>
-            <label><input type="radio" checked="checked"
-              name="gender" value="male">
+            <label><input type="radio" 
+              name="gender" value="male" <?php if ($errors['gender']) {print 'class="error"';} ?> <?php if ($values['gender']=='male') {print 'checked';}?>>
               Муж</label>
             <label><input type="radio"
-              name="gender" value="female">
+              name="gender" value="female" <?php if ($errors['gender']) {print 'class="error"';} ?> <?php if ($values['gender']=='female'){ print 'checked';}?>>
               Жен</label><br>
         </li>
         <li>
           <label>Ваш любимый язык программирования:</label><br>
-          <select multiple="multiple" name="favourite_lan[]"   <?php if ($errors['language']) {print 'class="error"';} ?> value="<?php print $values['language']; ?>"id="program_language">
-              <option value="1">Pascal</option>
-              <option value="2">C</option>
-              <option value="3">C++</option>
-              <option value="4">Java</option>
-              <option value="5">JavaScript</option>
-              <option value="6">PHP</option>
-              <option value="7">Python</option>
-              <option value="8">Haskell</option>
-              <option value="9">Clojure</option>
-              <option value="10">Prolog</option>
-              <option value="11">Scala</option>
+          <select multiple="multiple" name="favourite_lan[]"   <?php if ($errors['language']) {print 'class="error"';} ?> id="program_language">
+              <option value="1" <?php if(strpos($values['languages'],'1' )!== false && strpos($values['languages'],'10' )== false && strpos($values['languages'],'11' )=== false){print 'selected';} ?> >Pascal</option>
+              <option value="2" <?php if(strpos($values['languages'],'2' )!== false){print 'selected';} ?>>C</option>
+              <option value="3" <?php if(strpos($values['languages'],'3' )!== false){print 'selected';} ?>>C++</option>
+              <option value="4" <?php if(strpos($values['languages'],'4' )!== false){print 'selected';} ?>>Java</option>
+              <option value="5" <?php if(strpos($values['languages'],'5' )!== false){print 'selected';} ?>>JavaScript</option>
+              <option value="6" <?php if(strpos($values['languages'],'6' )!== false){print 'selected';} ?>>PHP</option>
+              <option value="7" <?php if(strpos($values['languages'],'7' )!== false){print 'selected';} ?>>Python</option>
+              <option value="8" <?php if(strpos($values['languages'],'8' )!== false){print 'selected';} ?>>Haskell</option>
+              <option value="9" <?php if(strpos($values['languages'],'9' )!== false){print 'selected';} ?>>Clojure</option>
+              <option value="10" <?php if(strpos($values['languages'],'10' )!== false){print 'selected';} ?>>Prolog</option>
+              <option value="11" <?php if(strpos($values['languages'],'11' )!== false){print 'selected';} ?>>Scala</option>
           </select>
         </li>
         <li>
             <label>
                 Биография<br>
-                <textarea name="biography"  <?php if ($errors['biography']) {print 'class="error"';} ?> value="<?php print  htmlspecialchars($values['biography']); ?>"
-                  placeholder="Расскажите о себе"></textarea>
+                <textarea name="biography"  <?php if ($errors['biography']) {print 'class="error"';} ?> 
+                  placeholder="Расскажите о себе"><?php print  $values['biography']; ?></textarea>
             </label><br>
         </li>
         <li>
             <br>
-          <label><input type="checkbox"  <?php if ($errors['checkboxContract']) {print 'class="error"';} ?> value="<?php print $values['checkboxContract']; ?>"
+          <label><input type="checkbox"  <?php if ($errors['checkboxContract']) {print 'class="error"';} ?> <?php if($values['checkboxContract'])print 'checked'; ?>
             name="checkboxContract">
             С контрактом ознакомлен (а)</label><br>
         </li>
@@ -101,6 +102,7 @@ if (!empty($messages)) {
             <input type="submit" value="Сохранить">
         </li>
     </ol>
+
   </form>
   </body>
 </html>
