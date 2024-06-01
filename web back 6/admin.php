df6e33926ba6995b04742a88ecdc7694c1dd244f
valeria <pechkanovalera@gmail.com>
2024-06-01 21:58:50 +0300

    s

--- web back 6/admin.php
+++ web back 6/admin.php
@@ -23,8 +23,8 @@ include('data.php');
   }
 if (empty($_SERVER['PHP_AUTH_USER']) ||
     empty($_SERVER['PHP_AUTH_PW']) ||
-    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
+    $_SERVER['PHP_AUTH_USER'] != $adminLogin ||
+    md5($_SERVER['PHP_AUTH_PW']) != md5($adminPass)) {
   header('HTTP/1.1 401 Unanthorized');
   header('WWW-Authenticate: Basic realm="My site"');
   print('<h1>401 Требуется авторизация</h1>');
