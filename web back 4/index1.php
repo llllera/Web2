11f2849ba34676ef4e386a6a0276d28338ba052b
valeria <pechkanovalera@gmail.com>
2024-06-02 20:03:00 +0300

    f

--- web back 4/index1.php
+++ web back 4/index1.php
@@ -5,7 +5,7 @@
 .error {
   border: 2px solid red;
 }
-.sss{
+input[type="text"]{
   border: 2px solid red;
 }
     </style>
@@ -29,7 +29,7 @@ if (!empty($messages)) {
         <li>
             <label>
                 ФИО<br>
-                <input name="name" class = "sss"<?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>"
+                <input name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>"
                 type="text"
                   placeholder="Введите ваше ФИО">
               </label><br>
